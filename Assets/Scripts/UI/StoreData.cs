855217f9edc60a4f3163ea2f4445b8df6c5dc91d
TaloozaGames <80441141+TaloozaGames@users.noreply.github.com>
2023-05-12 14:51:17 -0700

    working on game data

--- Assets/Scripts/UI/StoreData.cs
+++ Assets/Scripts/UI/StoreData.cs
@@ -2,68 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Linq;
+using Unity.VisualScripting;
 
 public class StoreData : MonoBehaviour
 {
-    public GameObject[] SceneDataChange;
+    [SerializeField]
+     public GameObject[] SceneDataChange;
+    [SerializeField]
+     public float[] SceneDataDelete;
 
-    public struct LoadedData
-    {
-        public GameObject objs;
-        public int num;
-        public int Number;
-        public float FloatingNumber;
-        public bool TOF;
-    };
-    public List<LoadedData> GameList;
-
+    private GameObject[] SceneData;
     // Start is called before the first frame update
     void Start()
     {
-        DontDestroyOnLoad(gameObject);
         SceneDataChange = GameObject.FindGameObjectsWithTag("AddData");
-        //get all data 
-        for (int i = 0; i < SceneDataChange.Length; i++)
-        {
-            for (int j = 0;j < GameList.Count;j++)
-            {
-                if (SceneDataChange[i].gameObject.GetComponent<Slider>() == GameList[j].objs.GetComponent<Slider>())
-                {
-                    SceneDataChange[i].GetComponent<Slider>().value = GameList[j].FloatingNumber;
-                }
-
-            }
-        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        //upon scene change, check if same data can be sent out, send out if so
-
-
-        //update data
-    }
-
-    public void updateData(GameObject obj, int NUm)
-    {
-
-        //get all data 
-        for (int i = 0; i < SceneDataChange.Length; i++)
+        SceneDataChange = GameObject.FindGameObjectsWithTag("AddData");
+        SceneData = GameObject.FindGameObjectsWithTag("GameManager");
+        if(!(SceneData.Length > 1))
         {
-            LoadedData LoadData = new LoadedData();
-            LoadData.objs = obj;
-            if (obj.GetComponent<Slider>())
-            {
-                LoadData.FloatingNumber = obj.GetComponent<Slider>().value;
-                LoadData.num = NUm;
-            }
-
-            GameList.Add(LoadData);
+            DontDestroyOnLoad(gameObject);
         }
 
+
+    }
+public void SetData() //on a scene load set data. run script when settings is opened.
+    {
+        for (int i = 0; i < SceneDataChange.Length; i++)
+        {
+            SceneDataChange[i].GetComponent<Slider>().value = SceneDataDelete[i];
+        }
     }
 
+    public void GetData()
+    {
+        for (int i = 0; i < SceneDataChange.Length; i++)
+        {
+            SceneDataDelete[i] = SceneDataChange[i].GetComponent<Slider>().value;
+        }
+    }
 }
 
 
